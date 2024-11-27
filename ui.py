55d1b5984cb96af06050ed8b020a5fa8fa6a8ea6
MikeSKV <skvorodinmihail@gmail.com>
2024-11-27 20:38:06 +0300

    Added button_click and clear functions

--- ui.py
+++ ui.py
@@ -4,3 +4,13 @@ from calc_operations import (
     square_root, sine, cosine, floor_value, ceil_value,
     memory_add, memory_subtract, memory_clear, memory_recall
 )
+
+entry_text = None
+
+def on_button_click(value):
+    """Обрабатывает нажатие кнопок цифр и операций."""
+    entry_text.set(entry_text.get() + value)
+
+def on_clear():
+    """Очищает поле ввода."""
+    entry_text.set("")
