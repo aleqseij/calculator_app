786a6418cbbd67a6c0d30cadf30de761729b964d
MikeSKV <skvorodinmihail@gmail.com>
2024-11-27 20:36:40 +0300

    Imported functions

---
+++ ui.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+from calc_operations import (
+    addition, subtraction, multiplication, division, modulus, power,
+    square_root, sine, cosine, floor_value, ceil_value,
+    memory_add, memory_subtract, memory_clear, memory_recall
+)
